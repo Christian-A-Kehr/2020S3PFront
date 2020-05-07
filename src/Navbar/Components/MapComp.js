feb4badc4b0603bcc0c85af4fda4bc56015d9abb
CaKehr <cph-ck209@cphbusiness.dk>
2020-05-07 14:27:24 +0200

    Updated MapComp + fetch
    Thomas feedback

--- src/Navbar/Components/MapComp.js
+++ src/Navbar/Components/MapComp.js
@@ -4,20 +4,8 @@ import "./map.css";
 import { useForm } from "./useform";
 import { useFetch } from "./useFetch";
 
-// skal formentligt laves om så map = mapComp 
+
 export default function MapComp({ isLoggedIn }) {
-
-    return (
-        <div>
-            < Map />
-        </div>
-    );
-}
-
-
-var prev;
-function Map() {
-    // uses custom hook from useform.js
     const [values, setValues] =
         useForm(
             {
@@ -29,125 +17,88 @@ function Map() {
                 countryrecovered: ""
             });
 
-    // // fetch all countries via backEnd
-    const { data, loading } = useFetch("http://localhost:8080/2020S3PBack/api/country");
 
-    // use useEffect to see values in consol doing development and afterwords changes it to display all data
+    ////////////////////////////////UseEffect\\\\\\\\\\\\\\\\\\\\\\\\\\\            
+    //skal vise obj så lav en getCountry function som aktiveres ved click
+    useFetch("http://localhost:8080/2020S3PBack/api/country");
+
     useEffect(() => {
-        const onMouseover = e => {
+        const onMouseClik = e => {
             console.log(e.target.id);
+            // values.countryCode.setValues(e.taget.id);
         };
-        document.addEventListener('click', onMouseover);
+        document.addEventListener('click', onMouseClik);
         return () => {
-            document.removeEventListener("click", onMouseover);
+            document.removeEventListener("click", onMouseClik);
         };
-
-
-        //  
         //     console.log(values.countryCode);
         // }, [values.countryCode]);
     }, []);
-    // const eventClick = world.getElementById("svg2");
-    // eventClick.addEventListener("click", eventHandler, false)
+
+    /////////////////////////////////getCountry\\\\\\\\\\\\\\\\\\\\\\\
+    function getCountry(id) {
+        // Brug id til at lave fix på GB og vandmasserne her. bare sæt egen værdier i returnpoints
+        console.log(id)
+        if (id == "svg2") {
+            // // // returnpoints skal laves om til at pege på hook   
+            // returnPoint1.innerText = "Name: Ocean"
+            // returnPoint2.innerText = "Population: alot "
+            // returnPoint3.innerText = "Area: less than 510.100.000 km2"
+            // returnPoint4.innerText = "Borders: incuding rivers? idk!"
+        } else {
+
+            var url = "http://restcountries.eu/rest/v1/alpha?codes=de" + id
+            console.log(url)
+            fetch(url)
+                .then(res => res.json())
+                .then(data => {
+                    // kontrol af data indhold
+                    console.log(data)
+                    // // then data =>{} = hvad skal der ske med dataen 
+                    // // returnpoints skal laves om til at pege på hook
+                    values.countryCode = data[0].alpha2Code
+                    values.countryName = data[0].name
+                    values.countryPopulation = data[0].population
+                    // values.countryInfcted = data[0].
+                    // returnPoint4.innerText = "Borders: " + data[0].borders
+                })
+        }
+    }
 
 
-
+    ////////////////////////eventHandler\\\\\\\\\\\\\\\\\\
+    var prev;
+    function eventHandler(e) {
+        // get id from event
+        var id = e.target.id;
+        console.log(id);
+        // paint or reset
+        if (!prev) {
+            e.target.style.fill = "red";
+            prev = e.target;
+        } else {
+            prev.style.fill = "rgb(192, 192, 192)";
+            e.target.style.fill = "red";
+            prev = e.target;
+        }
+        // getCountry henter daten og retuner det til returnPoints
+        getCountry(id);
+    }
+    ////////////////////////////////return MapComp\\\\\\\\\\\\\\\\\\\\\\\\\\\
     return (
-
         <div>
+            {/* Smid svg rent in her hvis det ikke snart virker... */}
             <div id="worldMap">
 
                 {/* not sure about img, in world its svg */}
-                <img src={world} alt="worldMap" />
-
-                <div>{loading ? 'loading...' : data}</div>
-                {/* < onClick={() => setCountry(e.target.id)} */}
-
+                <img src={world} alt="worldMap" onClick={eventHandler} />
             </div>
 
-
         </div>
-    )
+    );
 }
-
-function Clicked(props) {
-    var id = props.id;
-    return
-}
-
-function eventHandler(e) {
-    // get id from event
-    var id = e.target.id;
-    // paint or reset
-    if (!prev) {
-        e.target.style.fill = "red";
-        prev = e.target;
-    } else {
-        prev.style.fill = "rgb(192, 192, 192)";
-        e.target.style.fill = "red";
-        prev = e.target;
-    }
-    // getCountry henter daten og retuner det til returnPoints
-    // getCountry(id);
-}
-
-
-
-// // eventClick = hvor på kortet bliver der trykket på. 
-// const eventClick = document.getElementById("svg2");
-// // hvor vil vi placere daten vi henter (her navngivet som returnPoint (ref til index))
-// const returnPoint1 = document.getElementById("contryName");
-// const returnPoint2 = document.getElementById("contryPopulation");
-// const returnPoint3 = document.getElementById("contryArea");
-// const returnPoint4 = document.getElementById("contryBorders");
-
-
-function getCountry(id) {
-    // Brug id til at lave fix på GB og vandmasserne her. bare sæt egen værdier i returnpoints
-    console.log(id)
-    if (id == "svg2") {
-        // // // returnpoints skal laves om til at pege på hook   
-        // returnPoint1.innerText = "Name: Ocean"
-        // returnPoint2.innerText = "Population: alot "
-        // returnPoint3.innerText = "Area: less than 510.100.000 km2"
-        // returnPoint4.innerText = "Borders: incuding rivers? idk!"
-    } else {
-
-        var url = "http://restcountries.eu/rest/v1/alpha?codes=" + id
-        console.log(url)
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                // kontrol af data indhold
-                console.log(data)
-                // // then data =>{} = hvad skal der ske med dataen 
-                // // returnpoints skal laves om til at pege på hook   
-                // returnPoint1.innerText = "Name: " + data[0].name
-                // returnPoint2.innerText = "Population: " + data[0].population
-                // returnPoint3.innerText = "Area: " + data[0].area
-                // returnPoint4.innerText = "Borders: " + data[0].borders
-            })
-    }
-}
-
-// // eventListener registrere click på kortet og vidergiver event til eventHandler (Egen funktion)
-// eventClick.addEventListener("click", eventHandler, false)
-
-// // Var til at reset farver (skal være null til at begynde med, derfor er ingen værdi oplyst)
-// var prev;
-
-// function eventHandler(e) {
-//     // tag id givet fra event 
-//     var id = e.target.id;
-//     // giv farve hvis der ingen er ellers reset 
-//     if (!prev) {
-//         e.target.style.fill = "red";
-//         prev = e.target;
-//     } else {
-//         prev.style.fill = "rgb(192, 192, 192)";
-//         e.target.style.fill = "red";
-//         prev = e.target;
-//     }
-//     // getCountry henter daten og retuner det til returnPoints 
-//     getCountry(id);
-// }
+// options:
+// 1
+// // {addEventListener('click', eventHandler, false)}
+// 2
+// {eventHandler(Event)}
--- src/Navbar/Components/useFetch.js
+++ src/Navbar/Components/useFetch.js
@@ -3,14 +3,14 @@ import { useEffect, useState } from 'react';
 export const useFetch = (url) => {
     const [state, setState] = useState({ data: null, loading: true });
     useEffect(() => {
-        setState({ data: null, loading: true });
+        setState(state => ({ data: state.data, loading: true }));
         fetch(url)
             .then(x => x.json())
             .then(y => {
-                // problem med at return array
+                // problem med at return array/objct
                 // setState({ data: y, loading: false });
                 console.log(y);
             });
-    }, [url]);
+    }, [url, setState]);
     return state;
 };  
