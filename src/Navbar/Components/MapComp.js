752e2f0dd199e0b5a4a4cfa99d05bcd7f1af96f7
CaKehr <cph-ck209@cphbusiness.dk>
2020-05-08 14:15:04 +0200

    Map update
    Add. display data on right side of map (not complet)

--- src/Navbar/Components/MapComp.js
+++ src/Navbar/Components/MapComp.js
@@ -6,34 +6,44 @@ import { useFetch } from "./useFetch";
 
 
 export default function MapComp({ isLoggedIn }) {
-    const [values, setValues] =
-        useForm(
-            {
-                countryCode: "",
-                countryName: "",
-                countryPopulation: "",
-                countryInfcted: "",
-                countryDead: "",
-                countryrecovered: ""
-            });
+    const [countryCode, setcountrycd] = useState("");
+    const [countryName, setCountryName] = useState("");
+    const [countryPopulation, setcountryPopulation] = useState("");
+    const [countryInfected, setcountryInfected] = useState("");
+    const [countryDead, setcountryDead] = useState("");
+    const [countryRecovered, setcountryRevored] = useState("");
+
+    var mapsvg = document.getElementById("worldMap")
+
+
+    // const [values, setValues] =
+    //     useForm(
+    //         {
+    //             countryCode: "",
+    //             countryName: "",
+    //             countryPopulation: "",
+    //             countryInfcted: "",
+    //             countryDead: "",
+    //             countryrecovered: ""
+    //         });
 
 
     ////////////////////////////////UseEffect\\\\\\\\\\\\\\\\\\\\\\\\\\\            
     //skal vise obj så lav en getCountry function som aktiveres ved click
     useFetch("http://localhost:8080/2020S3PBack/api/country");
 
-    useEffect(() => {
-        const onMouseClik = e => {
-            console.log(e.target.id);
-            // values.countryCode.setValues(e.taget.id);
-        };
-        document.addEventListener('click', onMouseClik);
-        return () => {
-            document.removeEventListener("click", onMouseClik);
-        };
-        //     console.log(values.countryCode);
-        // }, [values.countryCode]);
-    }, []);
+    // useEffect(() => {
+    //     const onMouseClik = e => {
+    //         console.log(e.target.id);
+    //         // values.countryCode.setValues(e.taget.id);
+    //     };
+    //     document.addEventListener('click', onMouseClik);
+    //     return () => {
+    //         document.removeEventListener("click", onMouseClik);
+    //     };
+    //     //     console.log(values.countryCode);
+    //     // }, [values.countryCode]);
+    // }, []);
 
     /////////////////////////////////getCountry\\\\\\\\\\\\\\\\\\\\\\\
     function getCountry(id) {
@@ -56,17 +66,39 @@ export default function MapComp({ isLoggedIn }) {
                     console.log(data)
                     // // then data =>{} = hvad skal der ske med dataen 
                     // // returnpoints skal laves om til at pege på hook
-                    values.countryCode = data[0].alpha2Code
-                    values.countryName = data[0].name
-                    values.countryPopulation = data[0].population
+                    // values.countryCode.setValues(data[0].alpha2Code)
+
+                    setcountrycd(data[0].alpha2Code)
+                    setCountryName(data[0].name)
+                    setcountryPopulation(data[0].population)
+
+                    // setValues(values.countryCode = data[0].alpha2Code)
+                    // values.countryCode.setValues = data[0].alpha2Code
+                    // values.countryName = data[0].name
+                    // values.countryPopulation = data[0].population
                     // values.countryInfcted = data[0].
                     // returnPoint4.innerText = "Borders: " + data[0].borders
+                    console.log("code: " + countryCode + " Name: " + countryName + " poulation : " + countryPopulation);
                 })
         }
     }
+    ////////////////////////////Dropdown box\\\\\\\\\\\\\\\\\\\
+    function dropdown() {
+        var x = document.getElementById("Demo");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
+    /////////////////////////populate table\\\\\\\\\\\\\\\\\\\\\
 
+    function populateTable(id) {
+        // 1. fetchCall(id)
+        // 2. insert data in table. alternativ create table loob 
+    }
 
-    ////////////////////////eventHandler\\\\\\\\\\\\\\\\\\
+    ////////////////////////eventHandler\\\\\\\\\\\\\\\\\\\\\\\\
     var prev;
     function eventHandler(e) {
         // get id from event
@@ -83,22 +115,74 @@ export default function MapComp({ isLoggedIn }) {
         }
         // getCountry henter daten og retuner det til returnPoints
         getCountry(id);
+        populateTable(id);
     }
     ////////////////////////////////return MapComp\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
     return (
         <div>
             {/* Smid svg rent in her hvis det ikke snart virker... */}
-            <div id="worldMap">
+            <div id="textBox">
+                <div>   <button onclick={dropdown} class="DropDownButton">Select country</button></div>
+                Country name:        {countryName} <br></br>
+                Population:          {countryPopulation} <br></br>
+                Infected:            {countryInfected} <br></br>
+                Deceased:            {countryDead} <br></br>
+                Recovered:           {countryRecovered} <br></br>
+                _____________________________________ <br></br>
+                <p>country development</p>
 
-                {/* not sure about img, in world its svg */}
-                <img src={world} alt="worldMap" onClick={eventHandler} />
+                <table class="table">
+                    <tr>
+                        {/* <th>Country</th> */}
+                        <th>Date</th>
+                        <th>Infected</th>
+                        <th>Deceased</th>
+                        <th>Recovered</th>
+                    </tr>
+                    <tr>
+                        {/* <td>DE</td> */}
+                        <td>No date</td>
+                        <td>No data</td>
+                        <td>No data</td>
+                        <td>No data</td>
+                    </tr>
+                    <tr>
+                        {/* <td>DK</td> */}
+                        <td>No date</td>
+                        <td>No data</td>
+                        <td>No data</td>
+                        <td>No data</td>
+                    </tr>
+                    <tr>
+                        {/* <td>Se</td> */}
+                        <td>No date</td>
+                        <td>No data</td>
+                        <td>No data</td>
+                        <td>No data</td>
+                    </tr>
+                </table>
+                _____________________________________ <br></br>
+                <p>Graf TBO</p>
             </div>
 
-        </div>
+            <div id="worldMap">
+                {/* not sure about img, in world its svg */}
+                <img src={world} alt="worldmap" onClick={eventHandler} />
+                {/* {mapsvg.addEventListener("click", eventHandler, false)} */}
+
+            </div>
+
+        </div >
+
     );
+
 }
 // options:
 // 1
 // // {addEventListener('click', eventHandler, false)}
 // 2
 // {eventHandler(Event)}
+
+//////////////////////Links\\\\\\\\\\\\\\\\\\
+// Accessing Object Values + objt loop: https://www.w3schools.com/js/js_json_objects.asp
